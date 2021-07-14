bc77041fb75bb838dc50523bb2d71438d2e45865
epsilonbzh <epsilonbzh@protonmail.com>
2021-07-14 16:20:05 +0200

    add ability to get remaining time

--- src/fr/epsilon/dinanbus/core/Stop.java
+++ src/fr/epsilon/dinanbus/core/Stop.java
@@ -1,5 +1,6 @@
 package fr.epsilon.dinanbus.core;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 /**
  * Stops are places that the bus serves.
@@ -51,6 +52,34 @@ public class Stop {
 	 */
 	public ArrayList<Integer> getStop_list() {
 		return this.schedule_list;
+	}
+	
+	/**
+	 * Get the time for the next bus stop
+	 * based on device's current time
+	 * @return remaining time in minutes
+	 */
+	public int getNext() {
+		ArrayList<Integer> remainingTime = new ArrayList<Integer>(this.schedule_list);
+		int currentTime = (LocalDateTime.now().getHour() *60) + LocalDateTime.now().getMinute();
+		for(int v : remainingTime) {
+			remainingTime.set(remainingTime.indexOf(v),v - currentTime);
+		}
+		int i = 0;
+		boolean found = false;
+		while(found == false && i < remainingTime.size()) {
+			if(remainingTime.get(i) > 0) {
+				found = true;
+			}else {
+				i++;
+			}
+		}
+		if(found) {
+			return remainingTime.get(i);
+		}else {
+			return 1440 - currentTime + this.schedule_list.get(0);
+		}
+		
 	}
 	/**
 	 * Printable text containing station information
